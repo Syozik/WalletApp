3fec8cc411c053fe545882d09eaea4a845381570
Serhii Rubanskyi <rubanskiysergiy@gmail.com>
2024-12-17 23:45:17 +0100

    Updated daily points calculation

--- src/utils/getPoints.ts
+++ src/utils/getPoints.ts
@@ -1,3 +1,23 @@
-export function getPoints():number{
-    return 456000;
+function getDays(): number {
+    const today: Date = new Date();
+    const month: number = today.getMonth();
+    const seasonMonth: number = (month + 2) % 3 === 0 ? 3 : (month + 2) % 3;
+    let startMonth: number = month + 1 - seasonMonth;
+    let days: number = 0;
+
+    while (startMonth < month) {
+        days += new Date(today.getFullYear(), startMonth + 1, 0).getDate();
+        startMonth++;
+    }
+
+    return days + today.getDate();
+}
+
+export function getPoints(): number {
+    const days: number = getDays();
+    const result = [0,2,3];
+    for (let i=3;i<=days;i++){
+        result.push(result[i-2] + 0.6*result[i-1]);
+    }
+    return result[days];
 }
